addd79ee45d7ffc96d9660961fcf5ad31ea5143a
SALAH-EDDINE JABRI <gampodark120@gmail.com>
2025-03-19 10:32:29 +0000

    added based entity

---
+++ Models/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AssuranceSNTL.Models.Common;
+
+public class BaseEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid ID { get; set; }
+    public DateTimeOffset DateCreated { get; set; } = DateTimeOffset.Now;
+    public DateTimeOffset? DateUpdated { get; set; }
+    public DateTimeOffset? DateDeleted { get; set; }
+    public bool IsDeleted { get; set; } = false;
+}
