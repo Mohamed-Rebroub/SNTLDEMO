d14575a418c77186001c3beec97e8265e0c41c43
Mohamed REBROUB <srebroub550@gmail.com>
2025-03-19 11:45:47 +0000

    f8

---
+++ Repository/Interface/ITypeAssuranceRepo.cs
@@ -0,0 +1,14 @@
+﻿using AssuranceSNTL.Models;
+
+namespace AssuranceSNTL.Repository.Interface
+{
+    public interface ITypeAssuranceRepo
+    {
+        Task<IEnumerable<TypeAssurance>> GetAllAssurancesAsync();
+        Task<IEnumerable<TypeAssurance>> GetAllAssurancesById();
+
+        Task AddTypeAssuranceAsync(TypeAssurance typeAssurance);
+        Task UpdateTypeAssuranceAsync(TypeAssurance typeAssurance);
+        Task DeleteTypeAssuranceAsync(int id);
+    }
+}
