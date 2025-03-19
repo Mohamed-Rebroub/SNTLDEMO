8e7d0fc995c9e0bd3815204ae84e50a44cec3913
Mohamed REBROUB <srebroub550@gmail.com>
2025-03-19 10:33:49 +0000

    f5

---
+++ Models/PackAssurance.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace AssuranceSNTL.Models
+{
+    public class PackAssurance
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Nom { get; set; }
+
+        [Required, MaxLength(500)]
+        public string Description { get; set; }
+
+        public ICollection<PackTypeAssurance> PackTypeAssurances { get; set; }
+    }
+}
