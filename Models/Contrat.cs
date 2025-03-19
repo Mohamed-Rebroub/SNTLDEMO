19879b57eb9b68c613a98c764a157b5504d8458a
Mohamed REBROUB <srebroub550@gmail.com>
2025-03-19 12:04:51 +0000

    f10

--- Models/Contrat.cs
+++ Models/Contrat.cs
@@ -1,6 +1,23 @@
-﻿namespace AssuranceSNTL.Models
+﻿using AssuranceSNTL.Models.Common;
+
+namespace AssuranceSNTL.Models
 {
-    public class Contrat
+    public class Contrat : BaseEntity
     {
+        public int ContratId { get; set; }
+        public DateTime DateDebut { get; set; } 
+        public DateTime DateFin { get; set; } 
+        public decimal Montant { get; set; } 
+
+        public int PackAssuranceId { get; set; } 
+        public PackAssurance PackAssurance { get; set; } 
+
+        public int VoitureId { get; set; } 
+        public Voiture Voiture { get; set; } 
+
+        public int ClientId { get; set; } 
+        public Client Client { get; set; } 
+
+        public ICollection<TypeAssurance> TypeAssurances { get; set; } 
     }
 }
--- Models/ContratTypeAssurance.cs
+++ Models/ContratTypeAssurance.cs
@@ -1,6 +1,13 @@
-﻿namespace AssuranceSNTL.Models
+﻿using AssuranceSNTL.Models.Common;
+
+namespace AssuranceSNTL.Models
 {
-    public class ContratTypeAssurance
+    public class ContratTypeAssurance : BaseEntity
     {
+        public int ContratId { get; set; } 
+        public Contrat Contrat { get; set; } 
+
+        public int TypeAssuranceId { get; set; } 
+        public TypeAssurance TypeAssurance { get; set; } 
     }
 }
